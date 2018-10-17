d052b72d123deb389f46695eabceff0cd04dc423
Antoine Joly <jolyant@gmail.com>
2018-10-17 16:57:51 +0100

    fix

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('/dist'));
 
 app.listen(process.env.PORT || 4200);
 
 //Path Location Strategy
 
-app.get('/*', function(req, res) {
-res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile('/dist/hanzimanager/index.html');
 })
