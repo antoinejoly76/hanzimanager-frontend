ca996eefd30711165286ecfa2673118224e35e72
Antoine Joly <jolyant@gmail.com>
2018-10-17 17:21:17 +0100

    test

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
+
+
 const express = require('express');
 const app = express();
-const path = require('path');
-
-
-
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 4200);
-
-//Path Location Strategy
-
-app.get('/*', function (req, res) {
-    res.sendFile('/app/dist/hanzimanager/index.html');
-})
