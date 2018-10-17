5f05e137ae83425d291daa60c2c58b1eac701d80
Antoine Joly <jolyant@gmail.com>
2018-10-17 17:25:08 +0100

    try

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 4200);
+
+//Path Location Strategy
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
