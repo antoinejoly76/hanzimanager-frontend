a3696ad9038e0e0b1747bf13da07dda8c14c3173
Antoine Joly <jolyant@gmail.com>
2018-10-17 17:29:08 +0100

    fdsf

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 const express = require('express');
 const app = express();
+
+var path = require("path");
+
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
